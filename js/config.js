8954167f665fc843fe5ba6c58228a1f6e5837a10
Hikmatullah Sakti <hikmatullahsakti@gmail.com>
2026-08-03 13:44:49 +0700

    branding commit

--- js/config.js
+++ js/config.js
@@ -1,15 +1,19 @@
 /**
- * TIBYAN PRINT SERVICE v2.0
+ * MARKAZ PRINTING v2.0
+ * Powered by Buana Studios for Yayasan T.I.B.Y.A.N.
  * config.js - Client Configuration & API Endpoint Gateway
  */
 
 const APP_CONFIG = {
-  APP_NAME: 'Tibyan Print Service',
+  APP_NAME: 'Markaz Printing',
   VERSION: '2.0.0',
+  DEVELOPER: 'Buana Studios',
   ORGANIZATION: 'Yayasan T.I.B.Y.A.N.',
+  
   // Replace with your deployed Google Apps Script Web App URL for GitHub Pages REST API calls:
   // e.g., 'https://script.google.com/macros/s/AKfycbx.../exec'
-  API_URL: 'https://script.google.com/macros/s/AKfycbz1XQmXzGqpBsFER91F50K5pkife7N7OHp6Fx_Uz2t2RjAvBHcYR8UlPd_qg-xXacol/exec',
+  API_URL: '', 
+  
   MAX_FILE_SIZE_MB: 25,
   ALLOWED_EXTENSIONS: ['pdf', 'docx', 'doc', 'xlsx', 'xls', 'txt'],
   
